2bf44abe2085465eeb09b89078d9b962501bd90c
IscJlchavezG <iscjlchavez@hotmail.com>
2023-09-19 21:25:41 -0600

    login maquetado

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/dark.css">
+  <link rel="stylesheet" href="css/pace.css">
+  <script src="js/jquery.js"></script>
   <title>Inicio de sistema| IscjlchavezGAdmin</title>
 </head>
 <body>
@@ -27,24 +30,34 @@
                       </div>
                       <div class="row mt-2 mb-2 justify-content-center">
                           <div class="col-sm-8 col-md-8 col-lg-8">
-                            <input type="password" name="Password" id="password" placeholder="Password" class="form-control rounded-pill border-success" autocomplete="off">
+                            <input type="password" name="Password" id="pass" placeholder="Password" class="form-control rounded-pill border-success" autocomplete="off">
                           </div>
                       </div>
                       <div class="row mt-1 justify-content-center">
-                          <div class="col-sm-4 col-md-4 col-lg-4">
-                                texto
+                          <div class="col-sm-3 col-md-3 col-lg-3">
+                            <div class="form-check form-switch">
+                              <input class="form-check-input" type="checkbox" id="darkSwitch">
+                              <label class="darkSwitch" for="verPass">
+                                <svg class="bi" width="20" height="20" fill="currentColor">
+                                  <use xlink:href="library/icons/bootstrap-icons.svg#brightness-low-fill"/>
+                                </svg> |
+                                <svg class="bi" width="15" height="15" fill="currentColor">
+                                  <use xlink:href="library/icons/bootstrap-icons.svg#moon-stars-fill"/>
+                                </svg>
+                              </label>
+                            </div>
                           </div>
                           <div class="col-sm-3 col-md-3 col-lg-3">
                               <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" id="verPass">
-                                <label class="form-check-label" for="verPass">Ver Password</label>
+                                <input class="form-check-input" type="checkbox" id="ver" onclick="verpass(this);">
+                                <label class="form-check-label" for="ver">Ver Password</label>
                               </div>
                           </div>
                       </div>
                       <div class="row mt-4 mb-2 justify-content-center">
                           <div class="col-sm-8 col-md-8 col-lg-8">
                               <div class="d-grid gap-2">
-                                <input type="submit" name="btn-ingresar" id="btn-ingreso" class="btn btn-sm btn-outline-success rounded-pill">
+                                <input type="submit" name="btn-ingresar" id="btn-ingreso" class="btn btn-sm btn-outline-success bg-gradient rounded-pill">
                               </div>
                           </div>
                       </div>
@@ -76,5 +89,14 @@
         </div>
       </div>
       <script src="js/bootstrap.min.js"></script>
-    </body>
+      <script src="js/dark-mode.js"></script>
+      <script>
+          function verpass(cb) {
+              if (cb.checked)
+                $('#pass').attr("type", "text");
+              else
+                $('#pass').attr("type", "password");
+          }
+      </script>
+</body>
 </html>
