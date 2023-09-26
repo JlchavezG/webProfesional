01a8498118e2d11987ef9cbc40085c5799f878f1
IscJlchavezG <iscjlchavez@hotmail.com>
2023-09-25 23:41:34 -0600

    Dasboard

---
+++ Dashboard.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/dark.css">
+    <link rel="stylesheet" href="css/pace.css">
+    <script src="js/jquery.js"></script>
+    <title>Escritorio del sodium_crypto_secretstream_xchacha20poly1305_keygen| IscjlchavezGAdmin</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="justify-content-center mt-5">
+            <div class="row mt-3 text-center">
+                <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+                    <div class="row">
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <div class="card shadow border-0 border-3 border-start border-info bg-light">
+                                <div class="row py-2">
+                                    <div class="col">
+                                        <svg class="bi" width="25" height="25" fill="currentColor">
+                                            <use xlink:href="library/icons/bootstrap-icons.svg#person-workspace" />
+                                        </svg>
+                                    </div>
+                                    <div class="col">
+                                        <span class="fs-6">Usuarios </span><span class="fs-5 fw-bold">&nbsp 0</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <div class="card shadow border-0 border-3 border-start border-info bg-light">
+                                <div class="row py-2">
+                                    <div class="col">
+                                        <svg class="bi" width="25" height="25" fill="currentColor">
+                                            <use xlink:href="library/icons/bootstrap-icons.svg#mortarboard-fill" />
+                                        </svg>
+                                    </div>
+                                    <div class="col">
+                                        <span class="fs-6">Alumnos </span><span class="fs-5 fw-bold">&nbsp 0</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <div class="card shadow border-0 border-3 border-start border-info bg-light">
+                                <div class="row py-2">
+                                    <div class="col">
+                                        <svg class="bi" width="25" height="25" fill="currentColor">
+                                            <use xlink:href="library/icons/bootstrap-icons.svg#file-earmark-bar-graph-fill" />
+                                        </svg>
+                                    </div>
+                                    <div class="col">
+                                        <span class="fs-6">T.Reportes </span><span class="fs-5 fw-bold">&nbsp 0</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3 col-md-3 col-lg-3">
+                            <div class="card shadow border-0 border-3 border-start border-info bg-light">
+                                <div class="row py-2">
+                                    <div class="col">
+                                        <svg class="bi" width="25" height="25" fill="currentColor">
+                                            <use xlink:href="library/icons/bootstrap-icons.svg#file-earmark-bar-graph-fill" />
+                                        </svg>
+                                    </div>
+                                    <div class="col">
+                                        <span class="fs-6">T.Reportes </span><span class="fs-5 fw-bold">&nbsp 0</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
